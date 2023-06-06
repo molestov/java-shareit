8ffdc6dd74cd66f25498e2d83a641fbddceb8794
molestov <alexander@molestov.ru>
2023-06-06 22:13:03 +0500

    TZ 13 first commit

--- src/main/java/ru/practicum/shareit/user/storage/UserStorageImpl.java
+++ src/main/java/ru/practicum/shareit/user/storage/UserStorageImpl.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Component
 public class UserStorageImpl implements UserStorage {
